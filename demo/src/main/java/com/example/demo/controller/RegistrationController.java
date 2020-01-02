03149130f5c77ba05d70d7913c64b2b4243e4b3f
Lupachik <lupachik@gmail.com>
2020-01-03 01:27:48 +0300

    demo -- создание формы для редактирования

--- demo/src/main/java/com/example/demo/controller/RegistrationController.java
+++ demo/src/main/java/com/example/demo/controller/RegistrationController.java
@@ -7,6 +7,7 @@ import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
