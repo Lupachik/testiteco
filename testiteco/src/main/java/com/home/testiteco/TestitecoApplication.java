5fb84cc04520f67da8aae3fd785ea1f8ad98319d
Lupachik <lupachik@gmail.com>
2020-01-10 13:03:13 +0300

    2. новый проект дописал фронт энд

---
+++ testiteco/src/main/java/com/home/testiteco/TestitecoApplication.java
@@ -0,0 +1,13 @@
+package com.home.testiteco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestitecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestitecoApplication.class, args);
+	}
+
+}
